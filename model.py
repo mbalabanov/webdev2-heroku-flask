800648d31fb77c56514aad319836006d80382c9b
Marin Balabanov <marincomics@gmail.com>
2020-11-16 18:36:05 +0200

    Adaptations

--- model.py
+++ model.py
@@ -19,7 +19,7 @@ class User(db.Model):
 
     username = db.Column(db.String(255), nullable=False, unique=True)
     password_hash = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.Integer, unique=True)
+    email = db.Column(db.String, unique=True)
 
     session_cookie = db.Column(db.String(255), nullable=True, unique=True)
     session_expiry_datetime = db.Column(db.DateTime)
